b5263ef8fb2482199102538e88eeb42b8a29832c
MT <minhtruc25012004@gmail.com>
2025-10-30 20:52:07 +0700

    fix

--- Manage_Store/Models/Dtos/ResPaginationDto.cs
+++ Manage_Store/Models/Dtos/ResPaginationDto.cs
@@ -1,13 +1,13 @@
 namespace Manage_Store.Models.Dtos
 {
-    public class ResPagination
+    public class ResPagination<T>
     {
         public Meta meta { get; set; }
-        public T result;
+        public T result { get; set; }
         public class Meta
         {
             public int currentPage { get; set; }
-            public int  pageSize { get; set; }
+            public int pageSize { get; set; }
 
             public int totalPage { get; set; }
 
